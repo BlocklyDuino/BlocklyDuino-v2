8c8610f40fbb4fbb89fe32e5bce26eed95947a49
Kiran Payne <kiran.t.goel@gmail.com>
2021-05-22 13:29:47 -0400

    Fix bug where right clicking and getting/setting generic variables in iterators (i.e. for loop) were not generating code

--- blocklyduino/arduino/typed_variable.js
+++ blocklyduino/arduino/typed_variable.js
@@ -58,3 +58,15 @@ Blockly.Arduino['vars_get_boolean'] = function (block) {
     var code = Blockly.Arduino.variableDB_.getName(block.getFieldValue('VAR_GET_BOOLEAN'), Blockly.Variables.NAME_TYPE);
     return [code, Blockly.Arduino.ORDER_ATOMIC];
 };
+
+Blockly.Arduino['variables_set'] = function(block) {
+    var argument0 = Blockly.Arduino.valueToCode(block, 'VALUE', Blockly.Arduino.ORDER_ASSIGNMENT) || '0';
+    var varName = Blockly.Arduino.variableDB_.getName(block.getFieldValue('VAR'), Blockly.VARIABLE_CATEGORY_NAME);
+    return varName + ' = ' + argument0 + ';\n';
+};
+
+Blockly.Arduino['variables_get'] = function(block) {
+    var code = Blockly.Arduino.variableDB_.getName(block.getFieldValue('VAR'), Blockly.VARIABLE_CATEGORY_NAME);
+    return [code, Blockly.Arduino.ORDER_ATOMIC];
+};
+
